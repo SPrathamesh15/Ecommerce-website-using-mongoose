8f0dcaf7dca21da1335c872144bd3dcc91835cff
SPrathamesh15 <sprathamesh354@gmail.com>
2024-03-08 16:25:17 +0530

    Added delete functionality in cart

--- models/user.js
+++ models/user.js
@@ -37,7 +37,14 @@ userSchema.methods.addToCart = function(product){
             const updatedCart = { items: updatedCartItems};
             this.cart = updatedCart
             return this.save()
+}
 
+userSchema.methods.deleteItemFromCart = function(productId){
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString()
+    })
+    this.cart.items = updatedCartItems
+    return this.save()
 }
 module.exports = mongoose.model('User', userSchema)
 
